33c7b53bdb56c62b3e201745edfc5355e91b2b81
Bekzod Abilov <00009815@wiut.uz>
2021-03-26 18:03:52 +0500

    backend init

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended:false}))
 
 app.set('view engine', 'pug')
 
@@ -19,15 +21,58 @@ app.get('/create', (req, res) => {
 
 })
 
+app.post('/create', (req, res) => {
+	const title = req.body.title
+	const desc = req.body.desc
+
+	if(title.trim() == '' && desc.trim() == '') {
+		res.render('create', {error: true})
+	} else{
+		fs.readFile('./data/notes.json', (err, data) => {
+			if(err) throw err
+				let notes = JSON.parse(data)
+			notes.push({
+				id: id(),
+				title: title,
+				description: desc
+			})
+
+			fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+				if (err) throw err
+
+				res.render('create', {success: true})
+			})
+		})
+	}
+
+})
+
 app.get('/notes', (req, res) => {
-	res.render('notes', {n: notes})
+
+	fs.readFile('./data/notes.json', (err, data) => {
+		if(err) throw err
+
+		const notes = JSON.parse(data)
+
+		res.render('notes', {n: notes})
+	})
+	
 
 })
 app.get('/notes/:id', (req, res) => {
-	const noteID = req.params.id
-	const note = notes.filter(note => note.id == noteID)[0]
+	const id = req.params.id
 
-	res.render('detail', {note: note})
+	fs.readFile('./data/notes.json', (err, data) => {
+		if(err) throw err
+
+		const notes = JSON.parse(data)
+
+		const note = notes.filter(note => note.id == id)[0]
+
+		res.render('detail', {note: note})
+	
+})
+
 
 })
 
@@ -37,3 +82,7 @@ app.listen(8000, err => {
 
 		console.log('App is running...')
 })
+
+function id () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
